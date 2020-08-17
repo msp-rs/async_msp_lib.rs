3009fd58b9c45fdf17629ead2a316dc83b661409
Ilya Guterman <amfernusus@gmail.com>
2020-08-17 11:39:15 +0300

    use the buff_size as the write channel size, instead of static size

--- src/core.rs
+++ src/core.rs
@@ -27,7 +27,11 @@ impl Core {
     /// Create new core msp reader and parser
     pub fn new(buff_size: usize) -> Core {
         let (msp_reader_send, msp_reader_recv) = channel::<MspPacket>(4096);
-        let (msp_writer_send, msp_writer_recv) = channel::<MspPacket>(1024);
+        let write_buff_size = match buff_size {
+            0 => 1,
+            _ => buff_size,
+        };
+        let (msp_writer_send, msp_writer_recv) = channel::<MspPacket>(write_buff_size);
 
         let parser = MspParser::new();
         let parser_locked = Arc::new(Mutex::new(parser));
@@ -134,11 +138,8 @@ impl Core {
         task::spawn(async move {
             let (lock, cvar) = &*serial_write_lock;
 
-            let mut should_wait_for_lock = false;
             let temp_lock_guard = lock.lock().await;
-            if *temp_lock_guard > 0 {
-                should_wait_for_lock = true;
-            }
+            let should_wait_for_lock = *temp_lock_guard > 0;
             drop(temp_lock_guard);
 
             loop {
