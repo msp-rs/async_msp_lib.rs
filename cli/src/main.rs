74396eac40fc9a2684f9d91c2fb1aca5868200e1
Ilya Guterman <amfernusus@gmail.com>
2020-09-14 21:33:50 +0300

    cli: bump version to 0.1.12

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -119,7 +119,7 @@ impl std::io::Read for ClonableSerialPort {
 async fn main() {
 
     let matches = App::new("msp")
-        .version("0.1.11")
+        .version("0.1.12")
         .author("Ilya G. <amfernusus@gmail.com>")
         .about("Interact with msp flight controller")
         .subcommand(
